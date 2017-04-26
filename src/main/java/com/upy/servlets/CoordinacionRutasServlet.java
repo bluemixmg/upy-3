e2b1f31ca30a8a4fbf319678664497d16f089082
Oscar Saavedra <osaavedra@marnagarage.com>
2017-04-26 11:51:19 -0400

    removida la llamada a google.gson

--- src/main/java/com/upy/servlets/CoordinacionRutasServlet.java
+++ src/main/java/com/upy/servlets/CoordinacionRutasServlet.java
@@ -7,8 +7,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Random;
-
-import com.google.gson.JsonObject; 
+ 
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
